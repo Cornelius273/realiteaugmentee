bf81838be3427d66e2d5abd3fbb53b308d044a28
Cornelius273 <quentinseillier@Tardis.local>
2014-12-08 16:43:20 +0100

    Ajout du thème Garfunkel + modification du footer
    J’ai modifié le « footer.php » pour masquer les crédits du thème

---
+++ wp-content/themes/garfunkel/footer.php
@@ -0,0 +1,16 @@
+<div class="credits">
+
+	<p>
+	
+		&copy; <?php echo date("Y") ?> <a href="<?php echo home_url(); ?>" title="<?php bloginfo('name'); ?>"><?php bloginfo('name'); ?></a>. 
+	</p>
+
+<!-- /adieu les crédits de garfunkel ! les Daleks l'ont mangé :O  -->
+
+		
+</div> <!-- /credits -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
